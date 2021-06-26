f7eba07a73c232abf50b545f7b414ac16f1cd5a2
Marcus Fihlon <marcus@fihlon.swiss>
2021-06-26 16:03:20 +0200

    :recycle: Extracting sponsor form into own component using binder and events

--- src/main/java/org/komunumo/ui/view/admin/sponsors/SponsorDialog.java
+++ src/main/java/org/komunumo/ui/view/admin/sponsors/SponsorDialog.java
@@ -18,95 +18,38 @@
 
 package org.komunumo.ui.view.admin.sponsors;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Focusable;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.KeyModifier;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.component.textfield.TextField;
 import org.jetbrains.annotations.NotNull;
 import org.komunumo.data.db.enums.SponsorLevel;
 import org.komunumo.data.db.tables.records.SponsorRecord;
 import org.komunumo.data.service.SponsorService;
-import org.komunumo.ui.component.ImageUploadField;
-import org.komunumo.ui.component.KomunumoDatePicker;
+
+import java.util.List;
+
+import static org.komunumo.data.db.tables.Sponsor.SPONSOR;
 
 public class SponsorDialog extends Dialog {
 
-    private final Focusable<? extends Component> focusField;
+    private final SponsorService sponsorService;
 
     public SponsorDialog(@NotNull final SponsorRecord sponsor,
-                         final @NotNull SponsorService sponsorService) {
-        setCloseOnEsc(true);
+                         @NotNull final SponsorService sponsorService) {
+        this.sponsorService = sponsorService;
+
         setCloseOnOutsideClick(false);
 
-        final var title = new H2(sponsor.getId() == null ? "New sponsor" : "Edit sponsor");
-        title.getStyle().set("margin-top", "0");
+        final var form = new SponsorForm(List.of(SponsorLevel.values()));
+        form.addListener(SponsorForm.SaveEvent.class, event -> saveSponsor(event.getSponsor()));
+        form.addListener(SponsorForm.CancelEvent.class, event -> close());
+        form.setSponsor(sponsor);
 
-        final var nameField = new TextField("Name");
-        nameField.setRequiredIndicatorVisible(true);
-        nameField.setValue(sponsor.getName());
-
-        final var websiteField = new TextField("Website");
-        websiteField.setValue(sponsor.getWebsite());
-
-        final var levelField = new Select<>(SponsorLevel.values());
-        levelField.setLabel("Level");
-        levelField.setValue(sponsor.getLevel());
-
-        final var logoField = new ImageUploadField("Logo");
-        logoField.setValue(sponsor.getLogo());
-
-        final var validFromField = new KomunumoDatePicker("Valid from");
-        validFromField.setValue(sponsor.getValidFrom());
-
-        final var validToField = new KomunumoDatePicker("Valid to");
-        validToField.setValue(sponsor.getValidTo());
-
-        final var form = new FormLayout();
-        form.add(nameField, websiteField, levelField, logoField, validFromField, validToField);
-
-        final var saveButton = new Button("Save");
-        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        saveButton.addClickListener(clickEvent -> {
-            if (nameField.getValue().isBlank()) {
-                Notification.show("Please enter at least the name of the sponsor!");
-            } else if (validFromField.getValue() != null && validToField.getValue() != null
-                    && validFromField.getValue().isAfter(validToField.getValue())) {
-                Notification.show("The valid from date can't be after the valid to date!");
-            } else {
-                saveButton.setEnabled(false);
-                sponsor.setName(nameField.getValue());
-                sponsor.setWebsite(websiteField.getValue());
-                sponsor.setLogo(logoField.getValue());
-                sponsor.setLevel(levelField.getValue());
-                sponsor.setValidFrom(validFromField.getValue());
-                sponsor.setValidTo(validToField.getValue());
-                sponsorService.store(sponsor);
-
-                Notification.show("Sponsor saved.");
-                close();
-            }
-        });
-        saveButton.addClickShortcut(Key.ENTER, KeyModifier.CONTROL);
-        final var cancelButton = new Button("Cancel", clickEvent -> close());
-        final var buttonBar = new HorizontalLayout(saveButton, cancelButton);
-
-        add(title, form, buttonBar);
-
-        focusField = nameField;
+        add(form);
     }
 
-    @Override
-    public void open() {
-        super.open();
-        focusField.focus();
+    private void saveSponsor(@NotNull final SponsorRecord sponsor) {
+        sponsorService.store(sponsor);
+        Notification.show(String.format("Sponsor \"%s\" saved.", sponsor.get(SPONSOR.NAME)));
+        close();
     }
 }
