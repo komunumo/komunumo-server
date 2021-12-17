9ce4ab103e9908e73f6a7aadd7ae5128344ca011
Marcus Fihlon <marcus@fihlon.swiss>
2021-12-17 11:15:29 +0100

    :card_file_box: Add service class for newsletter registration #157

---
+++ src/main/java/org/komunumo/data/service/NewsletterService.java
@@ -0,0 +1,45 @@
+/*
+ * Komunumo - Open Source Community Manager
+ * Copyright (C) Marcus Fihlon and the individual contributors to Komunumo.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.komunumo.data.service;
+
+import org.jetbrains.annotations.NotNull;
+import org.jooq.DSLContext;
+import org.komunumo.data.db.enums.NewsletterRegistrationStatus;
+import org.komunumo.data.db.tables.records.NewsletterRegistrationRecord;
+import org.springframework.stereotype.Service;
+
+import static org.komunumo.data.db.tables.NewsletterRegistration.NEWSLETTER_REGISTRATION;
+
+@Service
+@SuppressWarnings("ClassCanBeRecord")
+public class NewsletterService {
+
+    private final DSLContext dsl;
+
+    public NewsletterService(@NotNull final DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    public NewsletterRegistrationRecord newRegistration() {
+        final var registration = dsl.newRecord(NEWSLETTER_REGISTRATION);
+        registration.setStatus(NewsletterRegistrationStatus.OPT_IN);
+        return registration;
+    }
+
+}
