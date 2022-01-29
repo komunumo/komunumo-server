30167fd0d0bd3861890f7be73f2015001bd45b7d
Marcus Fihlon <marcus@fihlon.swiss>
2022-01-29 13:22:43 +0100

    :white_check_mark: Add newsletter subscription tests

---
+++ src/test/java/org/komunumo/ui/view/website/home/HomeViewTest.java
@@ -0,0 +1,65 @@
+/*
+ * Komunumo - Open Source Community Manager
+ * Copyright (C) Marcus Fihlon and the individual contributors to Komunumo.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.komunumo.ui.view.website.home;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.textfield.EmailField;
+import org.junit.jupiter.api.Test;
+import org.komunumo.data.service.DatabaseService;
+import org.komunumo.ui.AbstractKaribuTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.github.mvysny.kaributesting.v10.LocatorJ._assertOne;
+import static com.github.mvysny.kaributesting.v10.LocatorJ._click;
+import static com.github.mvysny.kaributesting.v10.LocatorJ._get;
+import static com.github.mvysny.kaributesting.v10.LocatorJ._setValue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HomeViewTest extends AbstractKaribuTest {
+
+    @Autowired
+    private DatabaseService databaseService;
+
+    @Test
+    void subscribeToNewsletterWithSuccess() {
+        final var correctEmailAddress = "test@komunumo.org";
+
+        UI.getCurrent().navigate(HomeView.class);
+        _assertOne(HomeView.class);
+
+        _setValue(_get(EmailField.class), correctEmailAddress);
+        _click(_get(Button.class, spec -> spec.withCaption("Subscribe")));
+        assertTrue(databaseService.getSubscription(correctEmailAddress).isPresent());
+    }
+
+    @Test
+    void subscribeToNewsletterWithIncompleteEmail() {
+        final var incompleteEmailAddress = "test@komunumo";
+
+        UI.getCurrent().navigate(HomeView.class);
+        _assertOne(HomeView.class);
+
+        _setValue(_get(EmailField.class), incompleteEmailAddress);
+        final var button = _get(Button.class, spec -> spec.withCaption("Subscribe"));
+        assertFalse(button.isEnabled());
+    }
+
+}
