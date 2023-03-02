b8b743db04f7b29dc3a44a1017add5c9a5b89249
Marcus Fihlon <marcus@fihlon.swiss>
2023-03-02 13:06:30 +0100

    🗃️ Configure jOOQ to use lowercase names without quotes

--- src/main/java/org/komunumo/JtafJooqConfiguration.java
+++ src/main/java/org/komunumo/JtafJooqConfiguration.java
@@ -19,6 +19,7 @@
 package org.komunumo;
 
 import org.jooq.conf.RenderNameCase;
+import org.jooq.conf.RenderQuotedNames;
 import org.jooq.conf.Settings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,7 +33,9 @@ public class JtafJooqConfiguration {
      */
     @Bean
     Settings jooqSettings() {
-        return new Settings().withRenderNameCase(RenderNameCase.UPPER);
+        return new Settings()
+                .withRenderNameCase(RenderNameCase.LOWER)
+                .withRenderQuotedNames(RenderQuotedNames.NEVER);
     }
 
 }
